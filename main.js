c967733a91934f69ac48a59697b575213aac1112
jrlucas1 <joaolucas.pl151@academico.ifsul.edu.br>
2023-06-09 10:39:25 -0300

    Atividade Three.JS

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { createSkyBox } from './skybox'
 
 
 // Criando a cena
 const scene = new THREE.Scene();
 const loader = new GLTFLoader();
+var keyboard = {};
+var figura;
+
+document.addEventListener('keydown', function (event) {
+    keyboard[event.keyCode] = true;
+});
+
+document.addEventListener('keyup', function (event) {
+    keyboard[event.keyCode] = false;
+});
 
 // Criando a câmera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -17,15 +28,11 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 // Criando a SkyBox
-const skyBoxGeometry = new THREE.BoxGeometry(1000, 1000, 1000);
-const skyBoxMaterial = new THREE.MeshBasicMaterial({
-  map: new THREE.TextureLoader().load('ceu.jpg'),
-  side: THREE.BackSide
-});
-const skyBox = new THREE.Mesh(skyBoxGeometry, skyBoxMaterial);
 
-// Adicionando a SkyBox à cena
-scene.add(skyBox);
+createSkyBox('sky', 70).then(sky => {
+    console.log("O céu foi criado!")
+    scene.add(sky);
+})
 
 var geometry = new THREE.BoxGeometry();
 var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
@@ -36,6 +43,43 @@ function animate() {
     requestAnimationFrame(animate);
     controls.update();
 
+    switch (true) {
+        case keyboard[37]:
+            figura.position.x -= 0.1;
+            break;
+
+        case keyboard[39]:
+            figura.position.x += 0.1;
+            break;
+
+        case keyboard[38]:
+            figura.position.y += 0.1;
+            break;
+
+        case keyboard[40]:
+            figura.position.y -= 0.1;
+            break;
+        
+        case keyboard[65]:
+            figura.rotation.y += 0.02;
+            break;
+        
+        case keyboard[68]:
+            figura.rotation.y -= 0.02;
+            break;
+        
+        case keyboard[83]:
+            figura.rotation.x -= 0.02;
+            break;
+        
+        case keyboard[87]:
+            figura.rotation.x += 0.02;
+            break;
+
+        default:
+            break;
+    }
+
     renderer.render(scene, camera);
 }
 
@@ -44,20 +88,7 @@ function onWindowResize() {
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
 }
-var rotationSpeed = 0.02;
 
-function onKeyDown(event) {
-    switch (event.keyCode) {
-        case 37: // Left arrow key
-            cube.rotation.y -= rotationSpeed;
-            break;
-        case 39: // Right arrow key
-            cube.rotation.y += rotationSpeed;
-            break;
-    }
-}
-
-window.addEventListener('keydown', onKeyDown);
 
 window.addEventListener('resize', onWindowResize);
 
@@ -71,11 +102,12 @@ scene.add(light);
 
 loader.load('scene.gltf', function (gltf) {
     scene.add(gltf.scene);
+    figura = gltf.scene;
 }, undefined, function (error) {
     console.error(error);
 });
 
-camera.position.z = 5;
+camera.position.z = 8;
 
 
 
